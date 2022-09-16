7af69268049a32c437aaafb5acb0db50c4206850
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2022-09-16 08:37:37 +0200

    getPercentileChange modified for clarity

--- src/functions/getPercentileChange.ts
+++ src/functions/getPercentileChange.ts
@@ -2,31 +2,25 @@ import { NotAvailable } from "types";
 
 /**
  * calculate percentchange of two numbers (or strings, see return)
- * @param current
- * @param historical
+ * @param currentPrice
+ * @param historicalPrice
  * @returns string being representation of changes with plus/minus and percents mark. In case of being called with string return is 'n/a'string
  */
-function getPercentileChange(current: number, historical: number | NotAvailable): string | NotAvailable {
-  let result: string = "";
-  console.log(historical);
-  if (typeof historical === "number") {
-    const historicalPrice = historical;
-    let change = (100 * (current - historicalPrice)) / historicalPrice;
-    const change_string = change.toLocaleString("pl-PL", {
-      useGrouping: true,
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
+function getPercentileChange(currentPrice: number, historicalPrice: number | NotAvailable): string | NotAvailable {
+  if (typeof historicalPrice === "string") return historicalPrice;
 
-    if (change === 0) result = "no change";
-    else if (change > 0) result = `+${change_string}%`;
-    else if (change < 0) result = `${change_string}%`;
-  } else result = "n/a";
+  const change = (100 * (currentPrice - historicalPrice)) / historicalPrice;
+  const changeStringified = change.toLocaleString("pl-PL", {
+    useGrouping: true,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  let result: string = "";
+  if (change === 0) result = "no change";
+  else if (change > 0) result = `+${changeStringified}%`;
+  else if (change < 0) result = `${changeStringified}%`;
 
   return result;
 }
 export default getPercentileChange;
-
-/**
- * TODO: troche dziwnie decydowac po typie argumentu. Przychodzi "n/a" czasami. Zrobićwarunek if czy historical = NotAvailabe. Jeżeli tak to od razu return NotAvailable. Niepotrzebny ten result po drodze.
- */
