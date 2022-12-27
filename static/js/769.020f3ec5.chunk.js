240147014cdf512ed465e4fc25a4f8001907319b
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2022-12-27 10:38:42 +0100

    Updates

---
+++ static/js/769.020f3ec5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcryptocurrency_tracker=self.webpackChunkcryptocurrency_tracker||[]).push([[769],{769:function(e,n,r){r.r(n),r.d(n,{default:function(){return u}});var c=r(2791),t=r(7689),o=r(184),u=function(){var e=(0,t.s0)();return(0,c.useEffect)((function(){setTimeout((function(){e(-1)}),3e3)}),[]),(0,o.jsxs)("div",{className:"NoPage",children:[(0,o.jsxs)("h2",{children:['Ojejku! Strony o adresie "',decodeURIComponent(window.location.href),'" po prostu nie ma \ud83d\ude25']}),";"]})}}}]);
+//# sourceMappingURL=769.020f3ec5.chunk.js.map
