a2d61e18f946ecb38e5d55fc71a8014ebf57d6ae
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2023-08-03 10:58:05 +0200

    Updates

--- static/js/200.62829bbd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcryptocurrency_tracker=self.webpackChunkcryptocurrency_tracker||[]).push([[200],{200:function(r,c,e){e.r(c);var a=e(661),t=e(9172),n=e(184);c.default=function(r){var c=r.result,e=r.error,u=r.crypto,o=(0,a.UD)();return e&&o.error("Podczas pobierania danych bie\u017c\u0105cych dla ".concat(u," wyst\u0105pi\u0142 b\u0142\u0105d ")),c?null:(0,n.jsx)(t.aN,{})}}}]);
-//# sourceMappingURL=200.62829bbd.chunk.js.map
